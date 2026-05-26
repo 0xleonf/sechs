643d0b3f2920b073dd086c3e4e6ac5cff6e36acd
0xleonf <ucihasaskee07@gmail.com>
2026-05-27 01:56:11 +0800

    refactor: refactor handling errors (hope it much better)

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
-#include <locale>
 using namespace std;
 
-struct Error {
+struct Utils {
+  int length(char *argv[]) {
+    int i = 0, total = 0;
+    while (argv[1][i] != '\0') {
+      i++, total++;
+    }
+    return total;
+  }
 
+  void press() {
+    cout << "Press Enter to next...";
+    std::cin.clear();
+    std::cin.ignore(1000, '\n');
+    std::cin.get();
+  }
+
+  void clear_screen() { std::cout << "\033[2J\033[1;1H"; }
+};
+
+struct Error {
   void many_args(int &size) {
     if (size > 11)
-      cout << "Too many argument\n";
+      fprintf(stderr, "Too many argument\n");
   }
 
   void less_args(int &size) {
     if (size < 11)
-      cout << "Student ID must be exactly 11 characters long\n";
+      fprintf(stderr, "Student ID must be exactly 11 characters long\n");
   }
 
   bool prefix(char *argv[]) {
@@ -30,47 +49,97 @@ struct Error {
 
   void not_prefix(char *argv[]) {
     if (!prefix(argv)) {
-      cout << "Student ID must be F1D02\n";
+      fprintf(stderr, "Student ID must be start with F1D02\n");
+    }
+  }
+
+  int util_failed(Utils *util) {
+    if (util == NULL) {
+      fprintf(stderr, "failed to allocated memory\n");
+      exit(1);
+    }
+    return 0;
+  }
+
+  int error_failed(Error *error) {
+    if (error == NULL) {
+      fprintf(stderr, "failed to allocated memory\n");
+      exit(1);
+    }
+    return 0;
+  }
+
+  void input_failed(Utils *util) {
+    fprintf(stderr, "Invalid input! Try again: \n");
+
+    std::cin.clear();
+    std::cin.ignore(1000, '\n');
+
+    if (util != nullptr) {
+      util->press();
+      util->clear_screen();
     }
   }
 };
 
-int length(char *argv[]) {
-  int i = 0, total = 0;
-  while (argv[1][i] != '\0') {
-    i++, total++;
-  }
-  return total;
-}
-
-bool valid(int &size, char *argv[]) {
-  bool is_valid = true;
-  return is_valid;
-}
-
 int main(int argc, char *argv[]) {
+  Utils *util;
+  Error *error;
 
-  int size = length(argv);
+  error = (Error *)malloc(sizeof(*error));
+  util = (Utils *)malloc(sizeof(*util));
 
-  Error error;
-  error.many_args(size);
-  error.less_args(size);
-  error.not_prefix(argv);
+  error->util_failed(util);
 
+  int size = util->length(argv);
+
+  error->many_args(size);
+  error->less_args(size);
+  error->not_prefix(argv);
+  // free(error);
+  // free(util);
+
+  int option = -1;
   std::cout << R"(
-  ------------------------------------------------------------
-  Menu
-  ------------------------------------------------------------
-  1 - Show Historia's memories
-  2 - Show Mira's memories
-  3 - Show Victoria's memories
-  4 - Add memory to a sister
-  5 - Delete memory by index from a sister
-  6 - Print sisters' pool diagnostics
-  0 - Exit
-  ------------------------------------------------------------
-  Choose:
-    )";
+    ============================================================
+    SCHRYZA RESISTANCE, RECOVERY PROTOCOL [TERMINAL: PHOENIX]
+    ============================================================
+    You are CyroN's Memory Architect.
+    Heed the gods and heal the sisters.
+          )";
+  util->press();
+  util->clear_screen();
+
+  do {
+    std::cout << R"(
+    ------------------------------------------------------------
+      Menu
+    ------------------------------------------------------------
+        1 - Show Historia's memories
+        2 - Show Mira's memories
+        3 - Show Victoria's memories
+        4 - Add memory to a sister
+        5 - Delete memory by index from a sister
+        6 - Print sisters' pool diagnostics
+        0 - Exit
+    ------------------------------------------------------------
+            )";
+    std::cout << "Choose: ";
+    std::cin >> option;
+
+    if (std::cin.fail()) {
+      error->input_failed(util);
+      option = -1;
+      continue;
+    }
+
+    switch (option) {
+    case 1:
+      cout << "halo";
+      break;
+    }
+
+  } while (option != 0);
 
   return 0;
 }
