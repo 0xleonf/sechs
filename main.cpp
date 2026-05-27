00810f743aa72eef82390d00ea9b1fe8ac7f9224
0xleonf <ucihasaskee07@gmail.com>
2026-05-27 11:19:49 +0800

    refactor: move special gap handler

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
@@ -12,35 +13,94 @@ struct Utils {
     return total;
   }
 
-  int identify_last_digit(char *argv[]) {
-    int number;
-    if ((int)argv[1][10] % 2 != 0) {
-      number = 1;
-    } else {
-      number = 0;
+  int str_len(char *string) {
+    int i = 0, total = 0;
+    while (string[i] != '\0') {
+      i++, total++;
     }
-    return number;
+    return total;
   }
 
-  // converts 3 digit from char to int (dynamic allocation)
-  int convert_digit(char *argv[]) {
-    // base ascii 0
-    int base = 48;
-    int last_digit[3] = {(int)argv[1][8], (int)argv[1][9], (int)argv[1][10]};
+  // int identify_last_digit(char *argv[]) {
+  //   int number;
+  //   if ((int)argv[1][10] % 2 != 0) {
+  //     number = 1;
+  //   } else {
+  //     number = 0;
+  //   }
+  //   return number;
+  // }
+  //
+  // // converts 3 digit from char to int (dynamic allocation)
+  // int convert_digit(char *argv[]) {
+  //   // base ascii 0
+  //   int base = 48;
+  //   int last_digit[3] = {(int)argv[1][8], (int)argv[1][9], (int)argv[1][10]};
+  //
+  //   int size = sizeof(last_digit) / sizeof(last_digit[0]);
+  //   int *arr = (int *)malloc(size * sizeof(int));
+  //
+  //   if (arr != NULL) {
+  //     for (int i = 0; i < size; i++) {
+  //       arr[i] = (last_digit[i] - base);
+  //     }
+  //   }
+  //
+  //   int sum = ((arr[0] * 100) + (arr[1] * 10) + (arr[2]) * 1);
+  //
+  //   free(arr);
+  //   return (sum * 11 % 53) + 14;
+  // }
+  //
+  // int mira(char *argv[]) {
+  //   int base = 48;
+  //   int last_digit[3] = {(int)argv[1][8], (int)argv[1][9], (int)argv[1][10]};
+  //
+  //   int size = sizeof(last_digit) / sizeof(last_digit[0]);
+  //   int *arr = (int *)malloc(size * sizeof(int));
+  //
+  //   if (arr != NULL) {
+  //     for (int i = 0; i < size; i++) {
+  //       arr[i] = (last_digit[i] - base);
+  //     }
+  //   }
+  //
+  //   int sum = ((arr[0] * 100) + (arr[1] * 10) + (arr[2]) * 1);
+  //
+  //   free(arr);
+  //
+  //   return (sum * 7 % 23) + 12;
+  // }
 
-    int size = sizeof(last_digit) / sizeof(last_digit[0]);
-    int *arr = (int *)malloc(size * sizeof(int));
+  void strcopy(char *destination, const char *source) {
+    int i = 0;
+    while (source[i] != '\0') {
+      destination[i] = source[i];
+      i++;
+    }
+    destination[i] = '\0';
+  }
 
-    if (arr != NULL) {
-      for (int i = 0; i < size; i++) {
-        arr[i] = (last_digit[i] - base);
-      }
+  bool is_prime(int n) {
+    if (n <= 1)
+      return false;
+
+    for (int i = 2; i * i <= n; i++) {
+      if (n % i == 0)
+        return false;
     }
 
-    int sum = ((arr[0] * 100) + (arr[1] * 10) + (arr[2]) * 1);
+    return true;
+  }
 
-    free(arr);
-    return sum;
+  int generate_next_prime(int curr_prime) {
+    int number = curr_prime + 1;
+
+    while (is_prime(number)) {
+      number++;
+    }
+
+    return number;
   }
 
   void press() {
@@ -114,26 +174,97 @@ struct Error {
 };
 
 struct Memory {
-  int curr_offset;
-  int pool_size;
-  int special_gap;
-  int byte;
-
-  Utils *utils;
-  void add_memory(int &byte) {
-    int aligned_pos = curr_offset;
-    if (aligned_pos % byte != 0) {
-      aligned_pos = aligned_pos + (byte - (aligned_pos % byte));
-    }
-  }
+  char type[10];
+  size_t size;
+  uintptr_t address;
+  size_t jump;
+  char value[100];
 };
+
+struct Sister {
+  char name[10];
+  size_t pool_size;
+  size_t alignment;
+  size_t curr_offset;
+  int special_gap;
+  int curr_prime;
+
+  Memory memories[50];
+  int memory_count;
+
+  int historia(char *argv[]) {
+    int number;
+    if ((int)argv[1][10] % 2 != 0) {
+      number = 1;
+    } else {
+      number = 0;
+    }
+    return number;
+  }
+
+  // converts 3 digit from char to int (dynamic allocation)
+  int victoria(char *argv[]) {
+    // base ascii 0
+    int base = 48;
+    int last_digit[3] = {(int)argv[1][8], (int)argv[1][9], (int)argv[1][10]};
+
+    int size = sizeof(last_digit) / sizeof(last_digit[0]);
+    int *arr = (int *)malloc(size * sizeof(int));
+
+    if (arr != NULL) {
+      for (int i = 0; i < size; i++) {
+        arr[i] = (last_digit[i] - base);
+      }
+    }
+
+    int sum = ((arr[0] * 100) + (arr[1] * 10) + (arr[2]) * 1);
+
+    free(arr);
+    return (sum * 11 % 53) + 14;
+  }
+
+  int mira(char *argv[]) {
+    int base = 48;
+    int last_digit[3] = {(int)argv[1][8], (int)argv[1][9], (int)argv[1][10]};
+
+    int size = sizeof(last_digit) / sizeof(last_digit[0]);
+    int *arr = (int *)malloc(size * sizeof(int));
+
+    if (arr != NULL) {
+      for (int i = 0; i < size; i++) {
+        arr[i] = (last_digit[i] - base);
+      }
+    }
+
+    int sum = ((arr[0] * 100) + (arr[1] * 10) + (arr[2]) * 1);
+
+    free(arr);
+
+    return (sum * 7 % 23) + 12;
+  }
+
+  void display_sister(char *string) {
+    cout << " ------------------------------------------------------------\n";
+    std::cout << string;
+    cout << " ------------------------------------------------------------\n";
+    ;
+  }
+
+  // void add_memory(char *sister_name, char *value) {}
+};
+
+void main_menu() {}
 
 int main(int argc, char *argv[]) {
   Utils *util;
   Error *error;
+  // Memory *memory;
+  Sister *sister;
 
   error = (Error *)malloc(sizeof(*error));
   util = (Utils *)malloc(sizeof(*util));
+  // memory = (Memory *)malloc(sizeof(*memory));
+  sister = (Sister *)malloc((3) * sizeof(*sister));
 
   error->util_failed(util);
 
@@ -157,38 +288,44 @@ int main(int argc, char *argv[]) {
   util->press();
   util->clear_screen();
 
-  cout << util->convert_digit(argv);
+  // util->strcopy(sister[0].name, "Historia");
+  // cout << sister[0].name;
+  // cout << "historia: " << sister->historia(argv) << "\n"
+  //      << "mira: " << sister->mira(argv) << "\n"
+  //      << "victoria: " << sister->victoria(argv) << "\n";
 
-  // do {
-  //   std::cout << R"(
-  //   ------------------------------------------------------------
-  //     Menu
-  //   ------------------------------------------------------------
-  //       1 - Show Historia's memories
-  //       2 - Show Mira's memories
-  //       3 - Show Victoria's memories
-  //       4 - Add memory to a sister
-  //       5 - Delete memory by index from a sister
-  //       6 - Print sisters' pool diagnostics
-  //       0 - Exit
-  //   ------------------------------------------------------------
-  //           )";
-  //   std::cout << "Choose: ";
-  //   std::cin >> option;
-  //
-  //   if (std::cin.fail()) {
-  //     error->input_failed(util);
-  //     option = -1;
-  //     continue;
-  //   }
-  //
-  //   switch (option) {
-  //   case 1:
-  //     cout << "halo";
-  //     break;
-  //   }
-  //
-  // } while (option != 0);
+  do {
+    std::cout << R"(
+    ------------------------------------------------------------
+      Menu
+    ------------------------------------------------------------
+        1 - Show Historia's memories
+        2 - Show Mira's memories
+        3 - Show Victoria's memories
+        4 - Add memory to a sister
+        5 - Delete memory by index from a sister
+        6 - Print sisters' pool diagnostics
+        0 - Exit
+    ------------------------------------------------------------
+            )";
+    std::cout << "Choose: ";
+    std::cin >> option;
+
+    if (std::cin.fail()) {
+      error->input_failed(util);
+      option = -1;
+      continue;
+    }
+
+    // switch (option) {
+    // case 1:
+    //   // char type[3][20];
+    //   // util->strcopy(sister[0].name, "Historia");
+    //   // sister->memories->size = 32;
+    //   // sister->pool_size = 1024;
+    // }
+
+  } while (option != 0);
 
   return 0;
 }
